ce2f8b2f36b01414126a9d9faec51f49839f2659
Poly0n <adrian2005mag@hotmail.com>
2025-12-23 22:03:13 -0500

    Update main.cpp

--- R6_Recoil/main.cpp
+++ R6_Recoil/main.cpp
@@ -2,110 +2,56 @@
 #include <Windows.h>
 
 BOOL mouseMacro(int opt) {
-
-    POINT pos;
-
-    if (opt == 1) {
-
-        ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-
-        while (TRUE) {
-
-            SHORT tabKeyState = GetAsyncKeyState(VK_TAB);                  
-            if ((1 << 15) & tabKeyState)
-            {
-                break;
-            }
-                
-            if ((GetKeyState(VK_LBUTTON) & 0x80) && (GetKeyState(VK_RBUTTON) & 0x80) != 0)
-            {
-                GetCursorPos(&pos);
-                SetCursorPos(pos.x, pos.y + 10);
-                Sleep(25);
-            }
+    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+    
+    while (true) {
+        if (GetAsyncKeyState(VK_TAB) & 0x8000) {
+            break;
+        }
+        
+        if ((GetAsyncKeyState(VK_LBUTTON) & 0x8000) && 
+            (GetAsyncKeyState(VK_RBUTTON) & 0x8000)) {
             
+            INPUT input = {0};
+            input.type = INPUT_MOUSE;
+            input.mi.dy = opt;
+            input.mi.dwFlags = MOUSEEVENTF_MOVE;
+            SendInput(1, &input, sizeof(INPUT));
+            
+            Sleep(25);
         }
+        
+        Sleep(1);
     }
-
-    if (opt == 2) {
-
-        ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-
-        while (TRUE) {
-
-            SHORT tabKeyState = GetAsyncKeyState(VK_TAB);
-            if ((1 << 15) & tabKeyState)
-            {
-                break;
-            }
-
-            if ((GetKeyState(VK_LBUTTON) & 0x80) && (GetKeyState(VK_RBUTTON) & 0x80) != 0)
-            {
-                GetCursorPos(&pos);
-                SetCursorPos(pos.x, pos.y + 12);
-                Sleep(25);
-            }
-
-        }
-    }
-
-    if (opt == 3) {
-
-        ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-
-        while (TRUE) {
-
-            SHORT tabKeyState = GetAsyncKeyState(VK_TAB);  
-            if ((1 << 15) & tabKeyState)
-            {
-                break;
-            }
-
-            if ((GetKeyState(VK_LBUTTON) & 0x80) && (GetKeyState(VK_RBUTTON) & 0x80) != 0)
-            {
-                GetCursorPos(&pos);
-                SetCursorPos(pos.x, pos.y + 15);
-                Sleep(25);
-            }
-                
-            }
-        }
-
-        return FALSE;
-    }
-
+    return true;
+}
     int main()
     {
         do {
+            ::ShowWindow(::GetConsoleWindow(), SW_SHOW);
             int option = 0; 
 
             std::cout << "----- [Recoil Controller] ----- \n\n";
 
-            std::cout << "Options:\n";
-            std::cout << "1. Low\n";
-            std::cout << "2. Medium\n"; 
-            std::cout << "3. High\n\n";
+            std::cout << "Reccomeneded options:\n";
+            std::cout << "1 - 5 Low\n";
+            std::cout << "6 - 10 Medium\n"; 
+            std::cout << "11 - 20 High\n\n";
 
             std::cout << "[i] Press 'TAB' to stop the macro\n";
             std::cout << "--->";    
             std::cin >> option; 
 
-            if (option == 1) {
-                mouseMacro(1);
-            }
-            else if (option == 2) {
-                mouseMacro(2);
-            }
-            else if (option == 3) {
-                mouseMacro(3);
-            }
-            else {
+            if (std::cin.fail()) {
                 std::cout << "\n[!] Wrong input";
                 return -1;
             }
+            else {
+                mouseMacro(option);
+            }
             std::system("cls");
 
-    } while (TRUE);
+    } while (true);
 
     return 0;
 }
